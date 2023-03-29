cb1aeda199b31ce320b7456357d29714c891353a
LudovicAns <ludovicanselin@gmail.com>
2023-03-29 10:57:46 +0200

    add nav to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Nav from "@/components/Nav/Nav";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Nav />
+        <Component {...pageProps} />
+      </>
+
+  )
 }

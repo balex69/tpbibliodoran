018671303ab9550cde08bcdceb4d40b185c98750
LudovicAns <ludovicanselin@gmail.com>
2023-03-29 15:30:10 +0200

    add stock page route

---
+++ pages/stock/index.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Stock.module.scss'
+
+export default function Stock() {
+    return(
+        <div className={styles.container}>
+            <h1>Page en développement</h1>
+        </div>
+    )
+}

9f35b3792a7bead2b4bbfacce61b5f1f03265344
LudovicAns <ludovicanselin@gmail.com>
2023-03-29 15:42:35 +0200

    fix div

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,8 +4,8 @@ import styles from './Card.module.scss'
 
 export default function Card({ children }: ChildProp) {
     return (
-        <dic className={styles.card}>
+        <div className={styles.card}>
             { children }
-        </dic>
+        </div>
     )
 }
